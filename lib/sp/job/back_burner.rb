60a3812a009d943fa80681d3baeed7cccbc2a5c7
José Pacheco <jose.pacheco@cldware.com>
2018-03-06 17:19:27 +0000

    Marretada to direct tests on production.

--- lib/sp/job/back_burner.rb
+++ lib/sp/job/back_burner.rb
@@ -306,6 +306,9 @@ $check_db_life_span = false
 $status_dirty       = false
 if $config[:postgres] && $config[:postgres][:conn_str]
   $pg = ::SP::Job::PGConnection.new(owner: $PROGRAM_NAME, config: $config[:postgres])
+  if $PROGRAM_NAME.split('/').last == 'saft-importer'
+    $pg.exec("SET log_min_duration_statement TO 0;")
+  end
   if $config[:options][:jsonapi] == true
     $jsonapi = SP::Duh::JSONAPI::Service.new($pg, ($jsonapi.nil? ? nil : $jsonapi.url), SP::Job::JobDbAdapter)
   end
