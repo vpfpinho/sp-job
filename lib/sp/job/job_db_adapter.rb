bcd8d2bee7c4cf62d84d46b2b55d2435f60ae35c
José Pacheco <jose.pacheco@cldware.com>
2018-07-20 17:39:56 +0100

    Generalizing names for paramenters.

--- lib/sp/job/job_db_adapter.rb
+++ lib/sp/job/job_db_adapter.rb
@@ -11,23 +11,23 @@ module SP
           def do_request_on_the_db(method, path, params)
             jsonapi_query = %Q[ SELECT * FROM public.jsonapi('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') ]
 
-            response = service.connection.exec jsonapi_query, method, (method == 'GET' ? url_with_params_for_query(path, params) : url(path)), (method == 'GET' ? '' : params_for_body(params)), user_id, company_id, company_schema, sharded_schema, accounting_schema, accounting_prefix
+            response = service.connection.exec jsonapi_query, method, (method == 'GET' ? url_with_params_for_query(path, params) : url(path)), (method == 'GET' ? '' : params_for_body(params)), user_id, entity_id, entity_schema, sharded_schema, subentity_schema, subentity_prefix
             response.first if response.first
           end
 
-          def explicit_do_request_on_the_db(exp_accounting_schema, exp_accounting_prefix, method, path, params)
+          def explicit_do_request_on_the_db(exp_subentity_schema, exp_subentity_prefix, method, path, params)
             jsonapi_query = %Q[ SELECT * FROM public.jsonapi('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') ]
 
-            response = service.connection.exec jsonapi_query, method, (method == 'GET' ? url_with_params_for_query(path, params) : url(path)), (method == 'GET' ? '' : params_for_body(params)), user_id, company_id, company_schema, sharded_schema, exp_accounting_schema, exp_accounting_prefix
+            response = service.connection.exec jsonapi_query, method, (method == 'GET' ? url_with_params_for_query(path, params) : url(path)), (method == 'GET' ? '' : params_for_body(params)), user_id, entity_id, entity_schema, sharded_schema, exp_subentity_schema, exp_subentity_prefix
             response.first if response.first
           end
 
-          def user_id           ; service.parameters.user_id ; end
-          def company_id        ; service.parameters.company_id ; end
-          def company_schema    ; service.parameters.company_schema.nil? ? nil : service.parameters.company_schema ; end
-          def sharded_schema    ; service.parameters.sharded_schema.nil? ? nil : service.parameters.sharded_schema ; end
-          def accounting_schema ; service.parameters.accounting_schema.nil? ? nil : service.parameters.accounting_schema ; end
-          def accounting_prefix ; service.parameters.accounting_prefix.nil? ? nil : service.parameters.accounting_prefix ; end
+          def user_id          ; service.parameters.user_id ; end
+          def entity_id        ; service.parameters.entity_id ; end
+          def entity_schema    ; service.parameters.entity_schema.nil? ? nil : service.parameters.entity_schema ; end
+          def sharded_schema   ; service.parameters.sharded_schema.nil? ? nil : service.parameters.sharded_schema ; end
+          def subentity_schema ; service.parameters.subentity_schema.nil? ? nil : service.parameters.subentity_schema ; end
+          def subentity_prefix ; service.parameters.subentity_prefix.nil? ? nil : service.parameters.subentity_prefix ; end
 
       end
 
