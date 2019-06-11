97e365aea205018c2a71fe618cdd26d25c5fda15
João Ramos <poor666@gmail.com>
2019-06-11 10:58:19 +0100

    fix typo

--- lib/sp/job/common.rb
+++ lib/sp/job/common.rb
@@ -415,11 +415,11 @@ module SP
               message.merge!({
                 wizard: td.current_job[:notification_options][:wizard],
                 wizard_options: td.current_job[:notification_options][:wizard_options]
-              }) td.current_job[:notification_options][:wizard] && td.current_job[:notification_options][:wizard_options]
+              }) if td.current_job[:notification_options][:wizard] && td.current_job[:notification_options][:wizard_options]
 
               message.merge!({
                 expiration_date: td.current_job[:notification_options][:expiration_date]
-              }) td.current_job[:notification_options][:expiration_date]
+              }) if td.current_job[:notification_options][:expiration_date]
             end
 
             notification_options = {
