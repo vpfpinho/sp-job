a9cc1c7787e99bb29d7b6eac69c64312bbe77efd
Andre Catita <hostmake@gmail.com>
2019-10-15 18:04:01 +0100

    job_data added to thread_data, so jruby points thread_job to it

--- lib/sp/job/back_burner.rb
+++ lib/sp/job/back_burner.rb
@@ -123,9 +123,10 @@ module SP
       end
     end
 
-    class ThreadData < Struct.new(:job_status, :report_time_stamp, :exception_reported, :job_id, :publish_key, :job_key, :current_job, :job_notification, :jsonapi, :job_tube, :notification_lock, :notification_lock_key, :tube_options)
+    class ThreadData < Struct.new(:job_status, :report_time_stamp, :exception_reported, :job_id, :publish_key, :job_key, :current_job, :job_notification, :jsonapi, :job_tube, :notification_lock, :notification_lock_key, :tube_options, :job_data)
       def initialize
         self.job_status = {}
+        self.job_data = {}
       end
     end
 
