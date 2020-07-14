6b21c54648b058ce866b844b2edd1c00c309c1ae
Joana Alexandra Neves <joana.alexandra.neves@gmail.com>
2020-07-14 16:50:53 +0100

    We are having ssl problems connecting to Rollbar

--- lib/sp/job/back_burner.rb
+++ lib/sp/job/back_burner.rb
@@ -570,6 +570,7 @@ unless $config[:rollbar].nil?
   end
 
   Rollbar.configure do |config|
+    config.verify_ssl_peer = false
     config.access_token = $config[:rollbar][:token] if $config[:rollbar][:token]
     config.environment  = $config[:rollbar][:environment] if $config[:rollbar] && $config[:rollbar][:environment]
     if 'jruby' == RUBY_ENGINE
