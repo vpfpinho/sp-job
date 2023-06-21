018f22b1dbff6e2ce434460fd189ede1b41bc2f7
Eurico Inocencio <eurico.inocencio@gmail.com>
2023-06-21 14:09:48 +0100

    Added util method to sanitize string against XSS

--- lib/sp/job/pg_connection.rb
+++ lib/sp/job/pg_connection.rb
@@ -338,6 +338,12 @@ module SP
         }
       end
 
+      #
+      # Returns a XSS safe version of the string, offending chars replaced by spaces
+      #
+      # @param str text to validate
+      # @return cleaned up string
+      #
       def xss_sanitize (str)
         connect()
         sanitized = nil
